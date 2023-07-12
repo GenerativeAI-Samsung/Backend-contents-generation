50addba7d86513469c0b805d56cbdacf34167f2e
LeQuocViet02 <lequocviet222000vip@gmail.com>
2023-07-12 15:04:45 +0700

    second commit

--- simple_flask.py
+++ simple_flask.py
@@ -1,5 +1,5 @@
-from flask import Flask, render_template, send_file
-
+from flask import Flask, render_template, send_file, jsonify
+import json
 # Create the Flask application
 app = Flask(__name__)
 
@@ -16,6 +16,16 @@ def video():
 def image():
     video_path = 'output/Dog.png'
     return send_file(video_path, mimetype='image/jpeg')
+
+# if __name__ == '__main__':
+#     app.run(host='0.0.0.0', port=5000)#, debug=True)
+
+
+@app.route('/api/name')
+def name():
+    with open("flask.json", encoding="utf-8") as f: #open file flash.json on sever
+        name = json.load(f)
+        return jsonify({"name": name})
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)#, debug=True)
