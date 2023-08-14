f602dc3f153576617d9d08c1b3a25d11fca09de9
ChuTuanDuc20182429 <duc.ct182429@sis.hust.edu.vn>
2023-08-14 10:10:20 +0700

    Upload CLIP script

--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
-blender ./virtual_studio/virtual_studio.blend --python ./scripts/main.py -- -fbx_file ./graphic_dtb/superman.fbx ./graphic_dtb/wolf.fbx ./graphic_dtb/lion.fbx
+blender ./virtual_studio/virtual_studio.blend --python ./scripts/main.py -- -fbx_file ./graphic_dtb/superman.fbx ./graphic_dtb/lion.fbx
 
